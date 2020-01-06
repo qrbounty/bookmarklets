fd8154cb889a6e316701f61b5c4ed9060aa2c104
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-06 10:59:15 -0800

    Create cookies.js

---
+++ src/cookies.js
@@ -0,0 +1,13 @@
+document.cookie = '';
+function a(b) {
+    c = /; /g;
+    return b.replace(c, '<br>');
+}
+if (document.cookie.length < 1) {
+    alert('No cookie from this site!')
+} else {
+    with((na = open('', '', '')).document) {
+        write(a('Cookie for ' + document.title.link(window.location.href) + ', dd. ' + new Date() + '<hr>' + document.cookie));
+        close()
+    }
+}
